95fbfa445745b8877b9abb66e640b6130ad7c6c7
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2026-03-16 16:58:26 +0530

    reactremoved

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { useAppContext } from "../context/AppContext"
 import { goalLabels, goalOptions } from "../assets/assets"
 import Card from "../components/ui/Card"
